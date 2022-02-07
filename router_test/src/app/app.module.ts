c22cd5fc695d0acba72336d0046ab95721d9738a
Anton Suslik <tonkomisko@gmail.com>
2022-02-07 12:00:57 +0100

    routing works now

--- router_test/src/app/app.module.ts
+++ router_test/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
